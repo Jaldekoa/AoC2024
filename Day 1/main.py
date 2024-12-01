96e52449971266d9215bcb38410a59d9827509ee
Jon Aldekoa <jaldekoa@gmail.com>
2024-12-01 18:52:10 +0100

    Day 1 complete

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,9 +1,20 @@
-def read_data(filename) -> tuple[int, int]:
+def read_data(filename):
     with open(filename, "r", encoding="utf-8") as f:
         input_data = [list(map(int, line.split())) for line in f]
     return zip(*input_data)
 
 
+def part_one(filename):
+    left, right = map(sorted, read_data(filename))
+    return sum(abs(l - r) for l, r in zip(left, right))
+
+
+def part_two(filename):
+    left, right = map(sorted, read_data(filename))
+    return sum(el * right.count(el) for el in left)
+
+
 if __name__ == "__main__":
-    left, right = map(sorted, read_data("input.txt"))
-    print(sum(abs(l - r) for l, r in zip(left, right)))
+    print(f"Part One: {part_one("input.txt")}")
+    print(f"Part Two: {part_two("input.txt")}")
+
